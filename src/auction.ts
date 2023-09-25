49e6ac6b5008d8b8bb7389c50a8524bd3dd47264
Reinis Martinsons <77973553+Reinis-FRP@users.noreply.github.com>
2023-09-26 02:52:06 +0300

    Reinis/dev 03 (#7)
    * wip: store auctions
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * wip: bids
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * fix
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    * wip
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>
    
    ---------
    
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- src/auction.ts
+++ src/auction.ts
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { BigNumber, constants as ethersConstants, utils as ethersUtils } from "ethers";
 import * as ss from "superstruct";
 
@@ -19,56 +20,146 @@ export const DepositStruct = ss.object({
 
 export type DepositData = ss.Infer<typeof DepositStruct>;
 
-export interface AuctionData {
+export interface AuctionBroadcastData {
   auctionId: string;
-  depositData: DepositData;
+  deposit: DepositData;
+  bidDeadline: number;
 }
 
-type EmitDeposit = (type: 'Deposit', dataType: AuctionData) => void;
+export const BidStruct = ss.object({
+  auctionId: ss.string(),
+  relayerAddress: ss.string(),
+});
+
+export type BidData = ss.Infer<typeof BidStruct>;
+
+interface AuctionData {
+  deposit: DepositData;
+  bids: Map<string, BidData>;
+}
+
+type EmitDeposit = (type: "Deposit", dataType: AuctionBroadcastData) => void;
 
 type EmitTypes = EmitDeposit; // TODO: Add EmitComplete.
-
-const isValidDepositData = (depositData: DepositData): boolean => {
-  try {
-    return (
-      ethersUtils.isAddress(depositData.recipient) &&
-      ethersUtils.isAddress(depositData.tokenAddress) &&
-      Number(depositData.destinationChainId) > 0 && // If this is not supported chain, will fallback to regular deposit.
-      BigNumber.from(depositData.amount).gte(0) && // Lower bound for uint256
-      BigNumber.from(depositData.amount).lte(ethersConstants.MaxUint256) && // Upper bound for uint256
-      BigNumber.from(depositData.relayerFeePct).gte(BigNumber.from(2).pow(63).mul(-1)) && // Lower bound for int64
-      BigNumber.from(depositData.relayerFeePct).lt(BigNumber.from(2).pow(63)) && // Upper bound for int64
-      BigNumber.from(depositData.quoteTimestamp).gte(0) && // Lower bound for uint32
-      BigNumber.from(depositData.relayerFeePct).lt(BigNumber.from(2).pow(32)) && // Upper bound for uint32
-      BigNumber.from(depositData.maxCount).gte(0) && // Lower bound for uint256
-      BigNumber.from(depositData.maxCount).lte(ethersConstants.MaxUint256) && // Upper bound for uint256
-      BigNumber.from(depositData.txValue).gte(0) && // Lower bound for uint256
-      BigNumber.from(depositData.txValue).lte(ethersConstants.MaxUint256) // Upper bound for uint256
-    );
-  } catch {
-    return false;
-  }
-};
 
 export class Auction {
   private bidWaitTimeMs: number;
   private emit: EmitTypes;
+  private auctions: Map<string, AuctionData>;
 
   constructor(config: AuctionConfig, emit: EmitTypes) {
     this.bidWaitTimeMs = config.bidWaitTimeMs;
     this.emit = emit;
+    this.auctions = new Map<string, AuctionData>();
   }
 
-  async deposit(depositData: DepositData): Promise<void> {
-    if (!isValidDepositData(depositData)) throw new Error("Invalid depositData");
+  async deposit(deposit: DepositData): Promise<void> {
+    if (!this.isValidDepositData(deposit)) throw new Error("Invalid deposit data");
+
+    const auctionId = this.generateAuctionId(deposit);
+
+    // Normally conflicts should not be possible as we delete concluded auctions at the end.
+    if (this.auctions.has(auctionId)) throw new Error("Conflicting auction is running");
+
+    this.auctions.set(auctionId, { deposit, bids: new Map<string, BidData>() });
+
+    const bidDeadline = new Date().getTime() + this.bidWaitTimeMs;
 
     // Announce auction to bidders.
-    this.emit('Deposit', { auctionId: "0x0", depositData });
+    this.emit("Deposit", { auctionId, deposit, bidDeadline });
 
     await this.sleep(this.bidWaitTimeMs);
+
+    const winningBid = this.endAuction(auctionId); // TODO: construct return data based on this.
+
+    // Delete the concluded auction so that no more bids can be accepted.
+    this.auctions.delete(auctionId);
+  }
+
+  async bid(bid: BidData): Promise<void> {
+    if (!this.isValidBidData(bid)) throw new Error("Invalid bid data"); // Also checks if auction exists (is open).
+
+    const auction = this.auctions.get(bid.auctionId);
+    assert(auction !== undefined); // Only for accessing bids below as validation checked the auction is open.
+    auction.bids.set(bid.relayerAddress, bid); // We allow relayer to change their bids.
+  }
+
+  private endAuction(auctionId: string): BidData | null {
+    const auction = this.auctions.get(auctionId);
+    assert(auction !== undefined, "Non-existing auction");
+
+    const winningBid = this.getRandomBid(auction.bids);
+    // TODO: add EmitComplete.
+
+    return winningBid;
+  }
+
+  private getRandomBid(bids: Map<string, BidData>): BidData | null {
+    if (bids.size === 0) return null; // Caller has to handle this.
+
+    const keysArray = Array.from(bids.keys());
+    const randomIndex = Math.floor(Math.random() * keysArray.length);
+    const randomBidder = keysArray[randomIndex];
+    const randomBid = bids.get(randomBidder);
+    assert(randomBid !== undefined);
+    return randomBid;
   }
 
   private sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  private isValidDepositData(deposit: DepositData): boolean {
+    try {
+      return (
+        ethersUtils.isAddress(deposit.recipient) &&
+        ethersUtils.isAddress(deposit.tokenAddress) &&
+        Number(deposit.destinationChainId) > 0 && // If this is not supported chain, will fallback to regular deposit.
+        BigNumber.from(deposit.amount).gte(0) && // Lower bound for uint256
+        BigNumber.from(deposit.amount).lte(ethersConstants.MaxUint256) && // Upper bound for uint256
+        BigNumber.from(deposit.relayerFeePct).gte(BigNumber.from(2).pow(63).mul(-1)) && // Lower bound for int64
+        BigNumber.from(deposit.relayerFeePct).lt(BigNumber.from(2).pow(63)) && // Upper bound for int64
+        BigNumber.from(deposit.quoteTimestamp).gte(0) && // Lower bound for uint32
+        BigNumber.from(deposit.relayerFeePct).lt(BigNumber.from(2).pow(32)) && // Upper bound for uint32
+        BigNumber.from(deposit.maxCount).gte(0) && // Lower bound for uint256
+        BigNumber.from(deposit.maxCount).lte(ethersConstants.MaxUint256) && // Upper bound for uint256
+        BigNumber.from(deposit.txValue).gte(0) && // Lower bound for uint256
+        BigNumber.from(deposit.txValue).lte(ethersConstants.MaxUint256) // Upper bound for uint256
+      );
+    } catch {
+      return false;
+    }
+  }
+
+  private generateAuctionId(data: DepositData): string {
+    const abiCoder = new ethersUtils.AbiCoder();
+    const encodedData = abiCoder.encode(
+      ["address", "address", "uint256", "uint256", "int64", "uint32", "bytes", "uint256", "uint256"],
+      [
+        data.recipient,
+        data.tokenAddress,
+        data.amount,
+        data.destinationChainId,
+        data.relayerFeePct,
+        data.quoteTimestamp,
+        data.message,
+        data.maxCount,
+        data.txValue,
+      ],
+    );
+
+    const encodedBytes = ethersUtils.arrayify(encodedData);
+    const hash = ethersUtils.keccak256(encodedBytes);
+
+    return hash.substring(0, 10); // Extract the first 4 bytes (8 characters) from the hash.
+  }
+
+  private isValidBidData(bid: BidData): boolean {
+    try {
+      // TODO: add signature verification.
+      return ethersUtils.isAddress(bid.relayerAddress) && this.auctions.has(bid.auctionId);
+    } catch {
+      return false;
+    }
   }
 }

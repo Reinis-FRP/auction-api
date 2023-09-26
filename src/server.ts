9c9893fc890b2d5a4eef1dcc40227107b65311c6
Reinis Martinsons <reinis@umaproject.org>
2023-09-26 13:52:08 +0300

    fix: event emitter
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,46 @@
-import http from 'http'
-import Events from 'events'
+import http from "http";
+import Events from "events";
 
-import * as express from './express'
-import * as ws from './ws'
-import * as auction from './auction'
+import * as express from "./express";
+import * as ws from "./ws";
+import * as auction from "./auction";
 
 // This retrns the express server so we can hook and test it using supertest
-export async function Server(env?:NodeJS.ProcessEnv){
-  const port = Number(env?.port ?? 2999)
-  const bidWaitTimeMs = Number(env?.bidWaitTimeMs ?? 60*1000)
+export async function Server(env?: NodeJS.ProcessEnv) {
+  const port = Number(env?.port ?? 2999);
+  const bidWaitTimeMs = Number(env?.bidWaitTimeMs ?? 60 * 1000);
 
-  const events = new Events()
-  const _auction = new auction.Auction({bidWaitTimeMs},(type,data)=>events.emit('message',type,data))
-  const app = express.ExpressApp(_auction)
+  const events = new Events();
+
+  const emitter: auction.EventEmitter = {
+    deposit: (data) => events.emit("message", "Deposit", data),
+    complete: (data) => events.emit("message", "AuctionComplete", data),
+  };
+
+  const _auction = new auction.Auction({ bidWaitTimeMs }, emitter);
+  const app = express.ExpressApp(_auction);
   const server = http.createServer(app);
-  const broadcast = ws.WsApp(server)
+  const broadcast = ws.WsApp(server);
 
   // listen to events from auction and broadcast to websocket
-  events.on('message',(type,data)=>{
-    try{
-    switch(type){
-      case 'Deposit':
-        broadcast.startAuction(data)  
-        break;
+  events.on("message", (type, data) => {
+    try {
+      const broadcastData = JSON.stringify({ type, data });
+      switch (type) {
+        case "Deposit":
+          broadcast.startAuction(broadcastData);
+          break;
+        case "AuctionComplete":
+          broadcast.endAuction(broadcastData);
+          break;
       }
-    }catch(err){
-      console.error(`Error broadcasting event: ${type} ${data}`)
+    } catch (err) {
+      console.error(`Error broadcasting event: ${type} ${data}`);
     }
-  })
+  });
   await new Promise((res) => {
     server.listen(port, () => res(app));
   });
-  console.log('Listening on ' + port)
-  return server
+  console.log("Listening on " + port);
+  return server;
 }
-
-
